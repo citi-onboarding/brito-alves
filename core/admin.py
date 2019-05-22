49d1ccd9541b7efb88eafe4d9df4602910adc981
vitor-amartins <mm.vitor.21@gmail.com>
2019-05-22 09:15:55 -0300

    feature/services-backend Developed the 'AreaDeAtuacao' model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import AreaDeAtuacao
 
-# Register your models here.
+
+@admin.register(AreaDeAtuacao)
+class AreaDeAtuacaoAdmin(admin.ModelAdmin):
+    list_display = ("title", "order",)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class AreaDeAtuacao(models.Model):
+    title = models.CharField("Título", max_length=25)
+    description = models.CharField("Descrição rápida", max_length=80)
+    text = models.TextField("Texto sobre a área")
+    image = models.ImageField("Imagem", null=True, blank=True)
+    order = models.IntegerField("Ordem de exibição")
+
+    class Meta:
+        ordering = ["order", "title"]
+        verbose_name = "Área de atuação"
+        verbose_name_plural = "Áreas de atuação"
+
+    def __str__(self):
+        return self.title
