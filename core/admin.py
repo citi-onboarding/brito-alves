082215713de0a38a1293f186d9843b37cde5be1d
Edu <eduardoaqz@gmail.com>
2019-05-22 22:20:56 -0300

    Developed the 'Associado' model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Associado
 
-# Register your models here.
+@admin.register(Associado)
+class AssociadoAdmin(admin.ModelAdmin):
+    list_display = ("name", "order",)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Associado(models.Model):
+    name = models.CharField("Nome", max_length=30, null=False, blank=False)
+    curriculum = models.CharField("Currículo", max_length=260, null=True, blank=True)
+    image = models.ImageField("Imagem", null=False, blank=False)
+    order = models.IntegerField("Ordem de exibição")
+
+    class Meta:
+        ordering = ["order", "name"]
+        verbose_name = "Associado"
+        verbose_name_plural = "Associados"
+
+    def __str__(self):
+        return self.name
