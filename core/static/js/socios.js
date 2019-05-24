9a2d89b760b4b0b25dd8ba551c6efc14b8d32718
Vitor Martins <vmam@cin.ufpe.br>
2019-05-24 12:04:03 -0300

    feature/partners-carousel Developed the basis of partners section

---
+++ core/static/js/socios.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  // $('.socios-row').on('init', function (event, slick) {
+  //   console.log('init');
+  //   let cards = $('.socios-card.slick-slide');
+  //   console.log(cards);
+  // });
+  $('.socios-row').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1050,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      }, {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          centerMode: true,
+        }
+      },
+    ],
+    nextArrow: `
+    <button class="arrow-btn arrow-right" type="button">
+        <img src="../static/assets/icons/arrow-right.svg" alt="Seta para o próximo slide do carrossel">
+    </button>
+    `,
+    prevArrow: `
+    <button class="arrow-btn arrow-left" type="button">
+        <img src="../static/assets/icons/arrow-left.svg" alt="Seta para o próximo slide do carrossel">
+    </button>
+    `,
+  });
+});
