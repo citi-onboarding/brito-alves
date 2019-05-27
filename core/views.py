6e3be38aef7f20cab98454f04f3ae32c61bc0e8e
Vitor Martins <vmam@cin.ufpe.br>
2019-05-27 14:59:48 -0300

    feature/partners-carousel Finished carousel development

--- core/views.py
+++ core/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from core.models import Associado
+
 
 def index(request):
-    return render(request,'index.html')
+    partners = Associado.objects.all()
+    return render(request, 'index.html', {'partners': partners})
