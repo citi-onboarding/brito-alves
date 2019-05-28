208ebd69212c595d6644c7ddf3052a88f6093131
Edu <eduardoaqz@gmail.com>
2019-05-28 00:14:58 -0300

    Assets files name changed

--- core/static/js/servicos.js
+++ core/static/js/servicos.js
@@ -4,12 +4,12 @@ $('#modal-btn').click(function() {
     $('#modal-slick').slick({
         nextArrow: `
     <button class="slick-next" type="button" style="display: block;">
-        <img class="slick-arrow" alt="right arrow" src="${static}/icons/right-arrow.svg">
+        <img class="slick-arrow" alt="right arrow" src="${static}/icons/arrow-right-modal.svg">
     </button>
 `,
         prevArrow: `
     <button class="slick-prev" type="button" style="display: block;">
-        <img class="slick-arrow" alt="left arrow" src="${static}/icons/left-arrow.svg">
+        <img class="slick-arrow" alt="left arrow" src="${static}/icons/arrow-left-modal.svg">
     </button>
 `,
     });
