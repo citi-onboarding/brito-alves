ae1f3b2d113791b035dd2202c8f2121882a1532d
vitor-amartins <vmam@cin.ufpe.br>
2019-06-03 15:07:49 -0300

    feature/navbar-development Fixed some bugs

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,7 +4,6 @@ const fechar = () => {
     const navbarMobile = document.querySelector('#nav .navbar-mobile');
     const navbarBtn = document.querySelector('#nav .navbar-btn');
     navbarMobile.style.display = 'none';
-    navbarBtn.style.position = 'absolute';
     opened = false;
 }
 
@@ -12,7 +11,6 @@ const abrir = () => {
     const navbarMobile = document.querySelector('#nav .navbar-mobile');
     const navbarBtn = document.querySelector('#nav .navbar-btn');
     navbarMobile.style.display = 'flex';
-    navbarBtn.style.position = 'fixed';
     opened = true;
 }
 
